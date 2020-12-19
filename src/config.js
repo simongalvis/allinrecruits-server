f9ce4a90b9ce0eee65e084ad3fb3bf558a7e4698
Simon Galvis <simongalvis9@gmail.com>
2020-12-19 11:52:11 -0700

    configure server

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://dunder_mifflin@localhost/allinrecruits",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://dunder_mifflin@localhost/allinrecruits-test" 
+  }
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const knex = require('knex')
+const app = require('./app')
+const { PORT, DATABASE_URL } = require('./config')
+
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+})
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.set('db', db)
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
