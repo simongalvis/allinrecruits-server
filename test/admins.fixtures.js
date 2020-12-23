06e93c9a6926c8d5fe2dd7a01dc801186ed131d7
Simon Galvis <simongalvis9@gmail.com>
2020-12-23 00:25:10 -0700

    Create admins fixtures

---
+++ test/admins.fixtures.js
@@ -0,0 +1,24 @@
+function makeAdminsArray() {
+    return [
+      {
+        id: 1,
+        fullname: 'Sam Gamgee',
+        username: 'sam.gamgee@shire.com',
+        password: 'secret',
+        date_created: '2029-01-22T16:28:32.615Z'
+        
+      },
+      {
+        id: 2,
+        fullname: 'Peregrin Took',
+        username: 'peregrin.took@shire.com',
+        password: 'secret',
+        date_created: '2100-05-22T16:28:32.615Z'
+        
+      }
+    ]
+  }
+  
+  module.exports = {
+    makeAdminsArray
+  }
